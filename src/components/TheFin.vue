44a6c97ef996758cc7c87d36903f458fb6a1b6df
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2024-04-22 14:53:18 +0700

    fix offsets TheFin
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- src/components/TheFin.vue
+++ src/components/TheFin.vue
@@ -2,7 +2,7 @@
   <footer
     class="relative flex flex-col items-center justify-center gap-6 px-5 py-8 pb-12 overflow-hidden section-fin text-light bg-primary-foreground"
   >
-    <p class="text-sm text-center opacity-75" data-aos="fade-down" data-aos-offset="400">
+    <p class="text-sm text-center opacity-75" data-aos="fade-down" data-aos-offset="100">
       Atas kehadiran doa dan restu dari Bapak/Ibu/saudara/i sekalian kami ucapkan termakasih.
     </p>
 
@@ -10,12 +10,12 @@
       class="text-4xl font-display"
       style="text-shadow: rgba(0, 0, 0, 0.1) 2px 2px 0px"
       data-aos="zoom-in"
-      data-aos-offset="400"
+      data-aos-offset="100"
     >
       Nanda &amp; Egi
     </h1>
 
-    <p class="text-sm opacity-75" data-aos="fade-up" data-aos-offset="400">Sabtu, 27 April 2024</p>
+    <p class="text-sm opacity-75" data-aos="fade-up" data-aos-offset="100">Sabtu, 27 April 2024</p>
 
     <img
       src="/ornaments/ornament-white-flower.png"
